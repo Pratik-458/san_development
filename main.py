d7c0dfa76a1ef9284854637b42a3fd70e602aa6f
Sarah-Masih <70310294+Sarah-Masih@users.noreply.github.com>
2023-05-13 12:45:18 +1000

    closed file after reading, added comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime 
 import re
 
+# generate time stamps for unique file name
 now = str(datetime.datetime.now())
 
 fileNumber = re.sub('[^a-zA-Z0-9 \n\.]', '', now)
@@ -18,11 +19,13 @@ def writeFile():
     f = open(fileName, "a")
     f.write("testing file: "+ fileNumber)
     f.close()
+    
     print("file created")
 
 def readFile():
     f = open(fileName, "r")
     print(f.read())
+    f.close()
     
 
 if __name__ == "__main__":
