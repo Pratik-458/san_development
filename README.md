e0ba24388b7384d6ca14a6ed4c3f9f2143ef16fd
adityan2851 <adhi2851@gmail.com>
2023-05-15 13:26:38 +1000

    Add Json

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ pip install PyAudio
 
 3. Run this file and say something when it indicates in terminal
 4. What you said has been recognized and displayed in the terminal
+
+
+Issues that I faced:
+https://stackoverflow.com/questions/33851379/how-to-install-pyaudio-on-mac-using-python-3
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import speech_recognition as sr
+import json
+import requests
 
 def main():
   r = sr.Recognizer()
+  # f = open("recognized.txt", "w")
 
   with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
@@ -10,6 +13,34 @@ def main():
 
     try:
       print("You have said: \n " + r.recognize_google(audio))
+      with open('recognized.txt', 'w') as f:
+        f.write(r.recognize_google(audio))
+
+      # Read the contents of the file
+      with open("recognized.txt", "r") as r:
+        read_contents = r.read()
+
+      data = {
+           "content": read_contents
+          }
+      json_data = json.dumps(data)
+      print(json_data)
+
+      with open("recognized.json","w") as j:
+        j.write(json_data)
+
+      # Send the JSON data to the API
+      # url = "http://localhost:3005/api"
+      # headers = {"Content-Type": "application/json"}
+      # response = requests.post(url, data=json_data, headers=headers)
+
+      # Check the response
+      # if response.status_code == 200:
+      #   print("File sent successfully!")
+      # else:
+      #   print("Error sending file: {}".format(response.text))
+      
+      f.close()
 
     except Exception as e:
       print("Error.. " + str(e))
