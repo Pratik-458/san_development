26bce1ff68de1087535073f5dd46bfa92be5cf07
indladineshgithub <127467831+indladineshgithub@users.noreply.github.com>
2023-05-26 23:54:29 -0400

    reverting the changes in dev branch

--- route/route.js
+++ route/route.js
@@ -51,8 +51,4 @@ router.post("/Signup", (req, res) => {
   controller.addUser(req, res);
 });
 
-router.get("/notes/search", (req, res) => {
-  controller.performSearch(req, res);
-});
-
 module.exports = router;
