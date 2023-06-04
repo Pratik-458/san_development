830b0146315d4b20d5fbe6c25836c61265a492e7
Pratik-458 <62799377+Pratik-458@users.noreply.github.com>
2023-06-04 13:59:33 +1000

    udpated unit tests

--- tests/route/updateNotes.js
+++ tests/route/updateNotes.js
@@ -1,5 +1,26 @@
 // 1. if there is no body provided we should return an error.
 
-const expect = require("chai").expect;
-const request = require("request");
-const { TESTING_URL } = require("../../constants/tests");
+import chai from "chai";
+import request from "es6-request";
+import { TESTING_URL } from "../../constants/tests.js";
+const { expect } = chai;
+const payload = {
+  title: "test",
+  email: "test",
+  description: "test",
+  noteId: "test",
+};
+
+describe("Update note API", () => {
+  describe("No body provided validation error", () => {
+    it("Status", (done) => {
+      request
+        .put(`${TESTING_URL}/api/notes`)
+        .send("")
+        .then(([_, response]) => {
+          expect(response.statusCode).to.equal(400);
+          done();
+        });
+    });
+  });
+});
